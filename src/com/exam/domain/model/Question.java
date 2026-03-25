a0a13a27d2565edef021023371486e7612001f74
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-03-25 00:54:42 +0000

    clase QuestionTypes

--- src/com/exam/domain/model/Question.java
+++ src/com/exam/domain/model/Question.java
@@ -1,16 +1,35 @@
 package domain;
 
+import com.exam.domain.vo.ValueObjects.AnswerText;
+import com.exam.domain.vo.ValueObjects.QuestionId;
+
+//Entidad abstracta que representa el concepto de pregunta en el dominio (ocp).
 public abstract class Question {
 
-    protected QuestionId id;
-    protected String text;
-    protected AnswerText correctAnswer;
+    protected final QuestionId id;
+    protected final String text;
+    protected final AnswerText correctAnswer;
 
-    public boolean isCorrect(AnswerText studenAnswer){
-
+    public Question(QuestionId id, String text, AnswerText correctAnswer){
+        this.id = id;
+        this.text = text;
+        this.correctAnswer= correctAnswer;
     }
 
-    public void displayFormat(){
+    public QuestionId getId(){
+        return id;
+    }
+
+    public String getText(){
+        return text;
+}
+
+    // Polimorfismo para evaluar respuestas segun el tipo de pregunta
+    public abstract boolean isCorrect(AnswerText studenAnswer);
+
+    
+    // Poliformismo para la presentacion visual
+    public abstract void displayFormat();
         
-    }
+    
 }
--- src/com/exam/domain/vo/ValueObjects.java
+++ src/com/exam/domain/vo/ValueObjects.java
@@ -9,12 +9,7 @@ public class ValueObjects {
                 
             }
         }
-        // public StudentId {
-        //     if (value == null || value.isBlank()) {
-        //         throw new IllegaArgumentException("El ID del estudiante no puede estar vacio.");
-                
-        //     }
-        // }
+        
     }
 
     public record QuestionId(String value) {
